1a7d1711a3ceeb131bb16b1660d7cfe503b74f94
MassGallo <maxnewyork15@gmail.com>
2024-09-17 10:28:48 -0400

    saving changes

--- Assets/Scripts/PlayerManaDisplay.cs
+++ Assets/Scripts/PlayerManaDisplay.cs
@@ -1,20 +1,58 @@
-using System;
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
 
 public class PlayerManaDisplay : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI manaText;
+    [SerializeField] private Image manaIcon;
+    [SerializeField] private Image noManaIcon;
+    [SerializeField] private Transform manaBarParent;
     [SerializeField] private Player player;
-    [SerializeField] private GameManager gameManager;
+    [SerializeField] private TextMeshProUGUI manaText;
 
-    public void InitializeManaDisplay(Player player) {
+    private List<Image> manaBar = new List<Image>();
+
+    public void InitializeManaDisplay(Player player)
+    {
         this.player = player;
+        Debug.Log("ManaDisplay initialized with player: " + player.name);
     }
 
     public void UpdateManaVisual()
     {
-        manaText.text = $"{player.GetMana()}/{player.GetTotalMana()}";
+        int currentMana = player.GetMana();
+        int totalMana = player.GetTotalMana();
+
+        // Adjust the number of mana icons
+        ResizeManaBar(totalMana);
+
+        // Update the visuals of each mana icon
+        for (int i = 0; i < totalMana; i++)
+        {
+            manaBar[i].sprite = (i < currentMana) ? manaIcon.sprite : noManaIcon.sprite;
+        }
+
+        // Update the mana text
+        manaText.text = $"{currentMana}/{totalMana}";
     }
 
+    private void ResizeManaBar(int totalMana)
+    {
+        // Add more mana icons if needed
+        while (manaBar.Count < totalMana)
+        {
+            Image newIcon = Instantiate(manaIcon, manaBarParent);
+            manaBar.Add(newIcon);
+        }
+
+        // Remove excess mana icons if needed
+        while (manaBar.Count > totalMana)
+        {
+            Image lastIcon = manaBar[manaBar.Count - 1];
+            manaBar.RemoveAt(manaBar.Count - 1);
+            Destroy(lastIcon.gameObject);
+        }
+    }
 }
+
